35b33289d009a86da7d724a6e2834a5055ce9450
tjcarney89 <tjcarney@stny.rr.com>
2017-04-04 11:31:48 -0400

    Add Game and Board Files

---
+++ HighwayBingo/Board.swift
@@ -0,0 +1,14 @@
+//
+//  Board.swift
+//  HighwayBingo
+//
+//  Created by TJ Carney on 4/4/17.
+//  Copyright © 2017 Oliver . All rights reserved.
+//
+
+import Foundation
+
+class Board {
+    
+    
+}
---
+++ HighwayBingo/Game.swift
@@ -0,0 +1,16 @@
+//
+//  Game.swift
+//  HighwayBingo
+//
+//  Created by TJ Carney on 4/4/17.
+//  Copyright © 2017 Oliver . All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Game {
+    
+    let board = Board()
+    let players = [FIRUser]()
+}
