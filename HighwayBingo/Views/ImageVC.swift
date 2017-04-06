de1a3d87f554bb2d02843d7e8f27af2453ee35a5
tjcarney89 <tjcarney@stny.rr.com>
2017-04-06 16:10:02 -0400

    Add Retake Picture

--- HighwayBingo/Views/ImageVC.swift
+++ HighwayBingo/Views/ImageVC.swift
@@ -9,6 +9,7 @@ class ImageViewController: UIViewController, UIImagePickerControllerDelegate, UI
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var loadingSpinner: UIActivityIndicatorView!
     
     var cellTitle: String = ""
     
@@ -16,9 +17,8 @@ class ImageViewController: UIViewController, UIImagePickerControllerDelegate, UI
     override func viewDidLoad() {
         super.viewDidLoad()
         print("CELL TITLE: \(cellTitle)")
+        loadingSpinner.isHidden = true
         statusLabel.isHidden = true
-        imageView.layer.borderWidth = 1
-        imageView.layer.borderColor = UIColor.black.cgColor
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
             let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
@@ -40,17 +40,26 @@ class ImageViewController: UIViewController, UIImagePickerControllerDelegate, UI
         guard let newImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {return}
         let smallImage = newImage.resized(withPercentage: 0.25)
         guard let imageData = UIImagePNGRepresentation(smallImage!) else {return}
+        loadingSpinner.startAnimating()
+        loadingSpinner.isHidden = false
+        statusLabel.isHidden = false
+        statusLabel.textColor = UIColor.black
+        statusLabel.text = "Thinking..."
         WatsonAPIClient.verifyImage(image: imageData) {
             let search = self.cellTitle
             print("SEARCH: \(search)")
             let lowercasedSearch = search.lowercased()
             for match in WatsonAPIClient.possibleMatches {
                 if match.contains(lowercasedSearch) {
+                    self.loadingSpinner.stopAnimating()
+                    self.loadingSpinner.isHidden = true
                     self.statusLabel.isHidden = false
                     self.statusLabel.text = "TRUE"
                     self.statusLabel.textColor = UIColor.green
                     break
                 } else {
+                    self.loadingSpinner.stopAnimating()
+                    self.loadingSpinner.isHidden = true
                     self.statusLabel.isHidden = false
                     self.statusLabel.text = "FALSE"
                     self.statusLabel.textColor = UIColor.red
@@ -60,6 +69,20 @@ class ImageViewController: UIViewController, UIImagePickerControllerDelegate, UI
         self.dismiss(animated: true, completion: nil)
         
     }
+    
+    @IBAction func retakeTapped(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.mediaTypes = [kUTTypeImage as String]
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+
+    }
+    
+    
 
     
 }
