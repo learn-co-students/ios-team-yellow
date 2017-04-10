3a396f7a6e02fdf7d2300abc95769e6feacb77a0
tjcarney89 <tjcarney@stny.rr.com>
2017-04-10 10:08:17 -0400

    Move Board Logic

--- HighwayBingo/Models/Board.swift
+++ HighwayBingo/Models/Board.swift
@@ -3,17 +3,66 @@
 ///
 
 import Foundation
+import GameKit
 
-class Board {
+enum BoardType: String {
+    case Highway, City, Tropical
+    
+    var images: [String] {
+        switch self {
+        case .Highway:
+            return ["building", "airport", "barn", "bicycle", "boat", "bus", "car", "gas station", "rv", "motel", "motorcycle", "police car", "power line", "restaurant", "restroom", "river", "silo", "subway", "telephone", "train", "truck", "animal", "stop sign", "billboard", "speed limit"]
+        case .City:
+            return ["ambulance", "bank", "bar", "bus", "car", "coffee", "crosswalk", "garbage", "hotel", "hydrant", "laundromat", "mailbox", "manhole", "map", "museum", "newspaper", "park", "parking meter", "police car", "free space", "statue", "stoplight", "taxi", "tourist", "vendor"]
+        case .Tropical:
+            return ["bathing suit", "beach chair", "beach", "boat", "coconut", "cooler", "flower", "frozen drink", "ice cream", "jet ski", "lei", "lifeguard", "palm tree", "rock", "sandals", "sandcastle", "seagull", "seashell", "sunglasses", "sunscreen", "surfboard", "towel", "umbrella", "waterfall", "free space"]
+        }
+    }
+}
+
+struct Board {
     
     var images: [String]
     var name: String
-    var boardID: Int
-    var individualID: Int?
+    var filled: [Int] = [13]
+    var winningCombos = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14 ,15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [1, 6, 11, 16, 21], [2, 7, 12, 17, 22], [3, 8, 13, 18, 23], [4, 9, 14, 19, 24], [5, 10, 15, 20, 25], [1, 7, 13, 19, 25], [5, 9, 13, 17, 21]]
+    //var individualID: Int?
     
-    init(images: [String], name: String, boardID: Int) {
-        self.images = images
-        self.name = name
-        self.boardID = boardID
+    init(boardType: BoardType) {
+        self.name = "\(boardType.rawValue) Bingo"
+        var _images = shuffle(images: boardType.images)
+        self.images = freeSpace(images: &_images)
+        
     }
+    
+    func checkForWin() -> Bool {
+        for combo in winningCombos {
+            let filledList = Set(filled)
+            let comboSet = Set(combo)
+            let winner = comboSet.isSubset(of: filledList)
+            if winner {
+                return true
+            }
+        }
+        return false
+    }
+    
 }
+
+
+
+
+
+func shuffle(images: [String]) -> [String] {
+    return GKRandomSource().arrayByShufflingObjects(in: images) as! [String]
+}
+
+func freeSpace(images: inout [String]) -> [String] {
+    for (index, image) in images.enumerated() {
+        if image == "free space" && index != 12 {
+            swap(&images[index], &images[12])
+        }
+    }
+    return images
+}
+
--- HighwayBingo/Views/BoardCollectionVC.swift
+++ HighwayBingo/Views/BoardCollectionVC.swift
@@ -20,42 +20,37 @@ class BoardCollectionVC: UIViewController, UICollectionViewDelegate, UICollectio
     @IBOutlet weak var collectionView: UICollectionView!
     
     
-    var board: Board?
+    var board: Board!
     //TODO: Move win logic to Game (or some other) class
-    var filled: [Int] = [13]
-    var winningCombos = [[1, 2, 3, 4, 5,], [6, 7, 8, 9, 10], [11, 12, 13, 14 ,15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [1, 6, 11, 16, 21], [2, 7, 12, 17, 22], [3, 8, 13, 18, 23], [4, 9, 14, 19, 24], [5, 10, 15, 20, 25], [1, 7, 13, 19, 25], [5, 9, 13, 17, 21]]
+    
     var selectedCell: BingoCollectionViewCell?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        createCityBingo()
-        shuffle()
-        freeSpace()
         collectionView.delegate = self
         collectionView.dataSource = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        checkForWin()
     }
-
-
+    
+    
     // MARK: UICollectionViewDataSource
-
-     func numberOfSections(in collectionView: UICollectionView) -> Int {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
-
-     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
         return 25
     }
     
     
-     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! BingoCollectionViewCell
         let name = board?.images[indexPath.item]
         if let name = name {
@@ -86,88 +81,26 @@ class BoardCollectionVC: UIViewController, UICollectionViewDelegate, UICollectio
     }
     
     
-    func checkForWin() {
-        for combo in winningCombos {
-            let filledList = Set(filled)
-            let comboSet = Set(combo)
-            let winner = comboSet.isSubset(of: filledList)
-            
-            if winner == true {
-                print("WINNER")
-                let winAlert = UIAlertController(title: "WINNER!", message: "You Won!", preferredStyle: .alert)
-                let action = UIAlertAction(title: "OK", style: .default, handler: nil)
-                winAlert.addAction(action)
-                self.present(winAlert, animated: true)
-            } else {
-                print("NO WINNER YET!")
-            }
-        }
-    }
-    
-    func shuffle() -> [String]? {
-        if let board = board {
-            board.images = GKRandomSource().arrayByShufflingObjects(in: board.images) as! [String]
-            return board.images
-        } else {
-            return nil
-        }
-        
-    }
-    
-    func freeSpace() {
-        if let board = board {
-            for (index, image) in board.images.enumerated() {
-                if image == "free space" && index != 12 {
-                    swap(&board.images[index], &board.images[12])
-                }
-            }
-        }
-    }
-    
-    func createHighwayBingo() {
-        let images = ["building", "airport", "barn", "bicycle", "boat", "bus", "car", "gas station", "rv", "motel", "motorcycle", "police car", "power line", "restaurant", "restroom", "river", "silo", "subway", "telephone", "train", "truck", "animal", "stop sign", "billboard", "speed limit"]
-        self.board = Board(images: images, name: "Highway Bingo", boardID: 1)
-    }
-    
-    func createCityBingo() {
-        let images = ["ambulance", "bank", "bar", "bus", "car", "coffee", "crosswalk", "garbage", "hotel", "hydrant", "laundromat", "mailbox", "manhole", "map", "museum", "newspaper", "park", "parking meter", "police car", "free space", "statue", "stoplight", "taxi", "tourist", "vendor"]
-        self.board = Board(images: images, name: "City Bingo", boardID: 2)
-    }
-    
-    func createTropicalBingo() {
-        let images = ["bathing suit", "beach chair", "beach", "boat", "coconut", "cooler", "flower", "frozen drink", "ice cream", "jet ski", "lei", "lifeguard", "palm tree", "rock", "sandals", "sandcastle", "seagull", "seashell", "sunglasses", "sunscreen", "surfboard", "towel", "umbrella", "waterfall", "free space"]
-        self.board = Board(images: images, name: "Tropical Bingo", boardID: 3)
-    }
-
-
 }
 
-    extension BoardCollectionVC : UICollectionViewDelegateFlowLayout {
+extension BoardCollectionVC : UICollectionViewDelegateFlowLayout {
     
     
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-            
-            let availableWidth = collectionView.frame.width / (itemsPerRow + 1)
-            
-            
-            let width = availableWidth
-            let height = width 
-            
-            return CGSize(width: width, height: height)
-            
-            
-        }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-//        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-//            return sectionInsets
-//            
-//        }
+        let availableWidth = collectionView.frame.width / (itemsPerRow + 1)
         
-//        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//            return sectionInsets.left
-//        }
-    
+        
+        let width = availableWidth
+        let height = width
+        
+        return CGSize(width: width, height: height)
+        
+        
     }
+    
+    
+}
 
 extension BoardCollectionVC {
     func updateCell(image: UIImage) {
@@ -176,7 +109,7 @@ extension BoardCollectionVC {
             cell.layer.borderColor = UIColor.green.cgColor
             cell.layer.borderWidth = 2
             cell.cellImageView.image = image
-            filled.append(cell.id)
+            board.filled.append(cell.id)
         }
         
     }
