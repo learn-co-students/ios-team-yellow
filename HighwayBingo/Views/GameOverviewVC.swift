7e0ae85d1ba969d507a5a58d144d9b1cc995a585
tjcarney89 <tjcarney@stny.rr.com>
2017-04-14 13:58:08 -0400

    Add Alert for Not Accepted Players

--- HighwayBingo/Views/GameOverviewVC.swift
+++ HighwayBingo/Views/GameOverviewVC.swift
@@ -15,6 +15,7 @@ class GameOverviewVC: UIViewController, UITableViewDelegate, UITableViewDataSour
     let waitingForLabel = UILabel()
     
     var currentUserIsLeader = false
+    var allPlayersAccepted = true
     
     var game: Game? {
         didSet {
@@ -25,6 +26,7 @@ class GameOverviewVC: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
         }
     }
+    
     
     var players = [Player]()
     
@@ -105,7 +107,17 @@ class GameOverviewVC: UIViewController, UITableViewDelegate, UITableViewDataSour
             // Border
             $0.purpleBorder()
             // Start game
-            $0.addTarget(self, action: #selector(self.startGame(_:)), for: UIControlEvents.touchUpInside)
+            for player in players {
+                if game?.participants[player.id] == false {
+                    allPlayersAccepted = false
+                }
+            }
+            if allPlayersAccepted == false {
+                $0.addTarget(self, action: #selector(self.displayAlert(_:)), for: .touchUpInside)
+            } else {
+                $0.addTarget(self, action: #selector(self.startGame(_:)), for: UIControlEvents.touchUpInside)
+            }
+            
             // Anchors
             $0.trailingAnchor.constraint(equalTo: margin.trailingAnchor).isActive = true
             $0.widthAnchor.constraint(equalTo: margin.widthAnchor).isActive = true
@@ -149,6 +161,17 @@ class GameOverviewVC: UIViewController, UITableViewDelegate, UITableViewDataSour
         sender.removeFromSuperview()
         guard let game = game else { return }
         FirebaseManager.shared.start(game: game)
+    }
+    
+    func displayAlert(_ sender: UIButton!) {
+        let alert = UIAlertController(title: "Start Game?", message: "Not everyone has accepted the invite", preferredStyle: .alert)
+        let yesAction = UIAlertAction(title: "Yes", style: .default) { (action) in
+            self.startGame(sender)
+        }
+        let noAction = UIAlertAction(title: "No", style: .default, handler: nil)
+        alert.addAction(yesAction)
+        alert.addAction(noAction)
+        self.present(alert, animated: true)
     }
 }
 
